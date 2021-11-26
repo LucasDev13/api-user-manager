7f8bb62af456486272a75f3a817462ee2afa4b6c
LucasDev13 <lucaspontes140@gmail.com>
2021-11-26 17:21:15 -0300

    Criado a camada de domain e iniciado a criação da camada de infra.

---
+++ Manager.Domain/Entities/User.cs
@@ -0,0 +1,64 @@
+﻿using Manager.Domain.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Domain.Entities
+{
+    public class User : Base
+    {
+        //Propriedades
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        // Usado pelo EF
+        public User()
+        {
+        }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+        }
+
+        //Comportamentos com validações de domínio.
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+            Validate();
+        }
+
+        //Autovalidação usando fluent validation
+        public override bool Validate()
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if (!validation.IsValid)
+            {
+                foreach (var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+                throw new Exception("Campos inválidos, por favor corrija-os!" + _errors[0]);
+            }
+            return true;
+        }
+    }
+}
---
+++ Manager.Infra/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Manager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Infra.Interfaces
+{
+    public interface IUserRepository: IBaseRepository<User>
+    {
+        //busca um email específico. Get: pega/obtem. busca o usuário com este email.
+        Task<User> GetByEmail(string email);
+        //busca úma coleção de emails que tenham parte do email. search: busca
+        Task<List<User>> SearchByEmail(string email);
+        //Busca uma coleção de nomes.
+        Task<List<User>> SearchByName(string name);
+    }
+}
