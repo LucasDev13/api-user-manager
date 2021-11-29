de5348059a3c998b602548878cd7edea585a4078
LucasDev13 <lucaspontes140@gmail.com>
2021-11-29 16:37:48 -0300

    Criando a camadade serviço.

--- Manager.Domain/Entities/User.cs
+++ Manager.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using Manager.Domain.Validators;
+﻿using Manager.Core.Exceptions;
+using Manager.Domain.Validators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Manager.Domain.Entities
 {
-    public class User : Base
+    public class User : Base  
     {
         //Propriedades
         public string Name { get; private set; }
@@ -25,6 +26,7 @@ namespace Manager.Domain.Entities
             Email = email;
             Password = password;
             _errors = new List<string>();
+            Validate();
         }
 
         //Comportamentos com validações de domínio.
@@ -56,7 +58,7 @@ namespace Manager.Domain.Entities
             {
                 foreach (var error in validation.Errors)
                     _errors.Add(error.ErrorMessage);
-                throw new Exception("Campos inválidos, por favor corrija-os!" + _errors[0]);
+                throw new DomainException("Campos inválidos, por favor corrija-os!", _errors);
             }
             return true;
         }
---
+++ Manager.Service/Services/UserService.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Manager.Core.Exceptions;
+using Manager.Domain.Entities;
+using Manager.Infra.Interfaces;
+using Manager.Service.DTO;
+using Manager.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IMapper mapper, IUserRepository userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserDTO> Create(UserDTO userDTO)
+        {
+            var userExists = await _userRepository.GetByEmail(userDTO.Email);
+
+            if (userExists != null)
+                throw new DomainException("Já existe um usuário cadastrado com o email informado");
+
+            var user = _mapper.Map<User>(userDTO);
+            user.Validate();
+
+            var userCreated = await _userRepository.Create(user);
+
+            return _mapper.Map<UserDTO>(userCreated);
+        }
+
+        public async Task<UserDTO> Update(UserDTO userDTO)
+        {
+            var userExists = await _userRepository.Get(userDTO.Id);
+
+            if (userExists != null)
+                throw new DomainException("Já existe um usuário cadastrado com o email informado");
+
+            var user = _mapper.Map<User>(userDTO);
+            user.Validate();
+
+            var userUpdate = await _userRepository.Update(user);
+
+            return _mapper.Map<UserDTO>(userUpdate);
+        }
+
+        public async Task Remove(long id)
+        {
+            await _userRepository.Remove(id);
+        }
+
+        public async Task<UserDTO> Get(long id)
+        {
+            var user = await _userRepository.Get(id);
+            return _mapper.Map<UserDTO>(user);
+        }
+
+        public async Task<List<UserDTO>> Get()
+        {
+            var allUsers = await _userRepository.Get();
+            return _mapper.Map<List<UserDTO>>(allUsers);
+        }
+
+        public async Task<List<UserDTO>> SearchByName(string nome)
+        {
+            var allUsers = await _userRepository.SearchByName(nome);
+            return _mapper.Map<List<UserDTO>>(allUsers);
+        }
+
+        public async Task<List<UserDTO>> SearchByEmail(string email)
+        {
+            var allUsers = await _userRepository.SearchByEmail(email);
+            return _mapper.Map<List<UserDTO>>(allUsers);
+        }
+
+        public async Task<UserDTO> GetByEmail(string email)
+        {
+            var user = await _userRepository.GetByEmail(email);
+            return _mapper.Map<UserDTO>(user);
+        }
+
+
+    }
+}
